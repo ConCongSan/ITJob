90384a911ade6c19c6d5e8cce8199534c4f7d5ed
ConCongSan <112951971+ConCongSan@users.noreply.github.com>
2023-02-21 15:46:28 +0700

    css-js-layout

---
+++ resources/views/layout/page-js.blade.php
@@ -0,0 +1,5 @@
+<script src="{{ url('assets/js/jquery.validate.min.js') }}"></script>
+<script src="{{ url('assets/libs/toastr/build/toastr.min.js') }}"></script>
+<script src="{{ url('assets/libs/sweetalert2/dist/sweetalert2.min.js') }}"></script>
+<script src="{{ url('assets/libs/DataTables/jquery.dataTables.min.js') }}"></script>
+<script src="{{ url('assets/libs/tinymce/tinymce.min.js') }}"></script>
