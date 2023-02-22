53c64e8a65d227ea1a7ca7d0ccc981aa243b8a1b
ConCongSan <112951971+ConCongSan@users.noreply.github.com>
2023-02-22 19:22:31 +0700

    menu

--- resources/views/trang-chu.blade.php
+++ resources/views/trang-chu.blade.php
@@ -1,4 +1,17 @@
-<div>
-    <a href="{{ route('user_Login') }}">Đăng nhập</a>
-    <a href="{{ route('user_Register') }}">Đăng ký</a>
-</div> 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="Description" content="Enter your description here"/>
+    @include('layout.page-css')
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Title</title>
+</head>
+<body>
+
+
+</body>
+@include('layout.page-js')
+</html>
